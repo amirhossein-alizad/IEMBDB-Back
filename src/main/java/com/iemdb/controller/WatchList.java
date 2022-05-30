0d4b1437964980e8ff04f519526c8443f9d1f1ca
pkootzari <pkootzari@gmail.com>
2022-05-30 10:16:12 +0430

    change user info getter in watchlist

--- src/main/java/com/iemdb/controller/WatchList.java
+++ src/main/java/com/iemdb/controller/WatchList.java
@@ -14,7 +14,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -31,13 +34,13 @@ public class WatchList {
     public ResponseEntity<List<Movie>> getWatchList() {
         Utils.wait(2000);
         try {
-            Optional<User> user = userRepository.findById(CurrentUser.username);
-            if(user.isEmpty())
-                throw new LoginRequired();
-            List<Movie> watchlist = user.get().getWatchList();
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            User user = (User) request.getAttribute("user");
+            List<Movie> watchlist = user.getWatchList();
             return new ResponseEntity<>(watchlist, HttpStatus.OK);
-        } catch (RestException e) {
-            return new ResponseEntity<>(null, e.getStatusCode());
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -45,38 +48,36 @@ public class WatchList {
     public ResponseEntity<String> addToWatchlist(@RequestBody Map<String, String> input) {
         Utils.wait(2000);
         try {
-            Optional<User> user = userRepository.findById(CurrentUser.username);
-            if(user.isEmpty())
-                throw new LoginRequired();
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            User user = (User) request.getAttribute("user");
             int movie_id = Integer.parseInt(input.get("movie_id"));
             Optional<Movie> movie = movieRepository.findById(movie_id);
             if(movie.isEmpty())
                 throw new MovieNotFound();
-            User user1 = user.get();
-            user1.addToWatchList(movie.get());
-            userRepository.save(user1);
+            user.addToWatchList(movie.get());
+            userRepository.save(user);
         } catch (RestException e) {
+            System.out.println(e);
             return new ResponseEntity<>(e.getMessage(), e.getStatusCode());
         } catch (Exception e) {
+            System.out.println(e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>("Movie has been added successfully!", HttpStatus.OK);
     }
-//
+
     @PostMapping("/watchlist/remove")
     public ResponseEntity<String> removeFromWatchlist(@RequestBody Map<String, String> input) {
         Utils.wait(2000);
         try {
-            Optional<User> user = userRepository.findById(CurrentUser.username);
-            if(user.isEmpty())
-                throw new LoginRequired();
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            User user = (User) request.getAttribute("user");
             int movie_id = Integer.parseInt(input.get("movie_id"));
             Optional<Movie> movie = movieRepository.findById(movie_id);
             if(movie.isEmpty())
                 throw new MovieNotFound();
-            User user1 = user.get();
-            user1.removeFromWatchList(movie.get());
-            userRepository.save(user1);
+            user.removeFromWatchList(movie.get());
+            userRepository.save(user);
             return new ResponseEntity<>("Movie removed successfully!", HttpStatus.OK);
         } catch (RestException e) {
             return new ResponseEntity<>(e.getMessage(), e.getStatusCode());
@@ -84,19 +85,15 @@ public class WatchList {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-//
+
     @GetMapping("/recommendations")
     public ResponseEntity<List<Movie>> getRecommendation() {
         Utils.wait(2000);
         try {
-            Optional<User> user = userRepository.findById(CurrentUser.username);
-            if(user.isEmpty())
-                throw new LoginRequired();
-            User user1 = user.get();
-            List<Movie> recommendations = user1.getUserRecommendations(StreamSupport.stream(movieRepository.findAll().spliterator(), false).collect(Collectors.toList()));
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            User user = (User) request.getAttribute("user");
+            List<Movie> recommendations = user.getUserRecommendations(StreamSupport.stream(movieRepository.findAll().spliterator(), false).collect(Collectors.toList()));
             return new ResponseEntity<>(recommendations, HttpStatus.OK);
-        } catch (RestException e) {
-            return new ResponseEntity<>(null, e.getStatusCode());
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
